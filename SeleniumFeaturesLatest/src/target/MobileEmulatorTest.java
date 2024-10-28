f788eb8860b901789d790b060ce550297c0bd154
anhctk <anh.chau515@gmail.com>
2024-10-28 13:37:15 +0700

    Anh 1st commit

--- SeleniumFeaturesLatest/src/target/MobileEmulatorTest.java
+++ SeleniumFeaturesLatest/src/target/MobileEmulatorTest.java
@@ -1,15 +1,11 @@
 package target;
 
-import org.openqa.selenium.chrome.ChromeDriver;
 
 public class MobileEmulatorTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-System.setProperty("webdriver.chrome.driver", "/Users/anhchau/Documents/chromedriver");
-ChromeDriver driver = new ChromeDriver();
-
+		System.out.println("test");
 
 	}
 
