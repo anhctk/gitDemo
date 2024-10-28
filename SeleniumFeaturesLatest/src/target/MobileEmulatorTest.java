7c75f7f9c9d02a1ccc57d410b6a560b0ce15855a
anhctk <anh.chau515@gmail.com>
2024-10-28 14:18:41 +0700

    Architech Asian Development

--- SeleniumFeaturesLatest/src/target/MobileEmulatorTest.java
+++ SeleniumFeaturesLatest/src/target/MobileEmulatorTest.java
@@ -6,6 +6,10 @@ public class MobileEmulatorTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("test");
+		System.out.println("test2");
+		System.out.println("test3");
+		System.out.println("test4");
+		System.out.println("test5");
 
 	}
 
